37c19bb258ede2e9f4a6d297d2fa2929629f1849
inayatulla <241637@astanait.edu.kz>
2025-09-26 15:16:15 +0500

    feat(mergesort): implement MergeSort with cutoff

--- src/main/java/algo/select/DeterministicSelect.java
+++ src/main/java/algo/select/DeterministicSelect.java
@@ -1,4 +1,59 @@
 package algo.select;
 
-public class DeterministicSelect {
+import algo.Arr;
+import algo.Metrics;
+
+public final class DeterministicSelect {
+    private final Metrics m;
+
+    public DeterministicSelect(Metrics m) { this.m = m; }
+
+    public int select( int[] a, int k ) {
+        if( a == null || a.length == 0 ) throw new IllegalArgumentException("empty");
+        if (k < 0 || k > a.length) throw new IllegalArgumentException("invalid k");
+        return select(a, 0, a.length -1 , k);
+    }
+
+    private int select( int[] a, int low, int high, int k) {
+        while (true) {
+            if (low == high) return a[low];
+
+            int pivot = medianOfMedians(a, low, high);
+            int p = partition()
+        }
+    }
+
+    private int medianOfMedians(int[] a, int low, int high) {
+        return
+    }
+
+    private void insertionSort(int[] a, int low, int high) {
+        for (int i = low + 1; i <= high; i++ ) {
+            int key = a[i], j = i - 1;
+
+            while (j >= low && Arr.cmp(m , a[j], key) > 0) {
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = key;
+        }
+    }
+    private int partition(int[] a, int low, int high, int pivotValue) {
+
+        int pvIndx = low;
+        while (pvIndx <= high && ) {
+
+        }
+        return
+    }
+
+    private void swap(int[] a, int i, int j) {
+        if (i != j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+
 }
