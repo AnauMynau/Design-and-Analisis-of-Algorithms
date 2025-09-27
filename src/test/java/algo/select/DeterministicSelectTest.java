549901a3b83834d76a55d8250a6569fb58bdd7dd
inayatulla <241637@astanait.edu.kz>
2025-09-27 21:25:37 +0500

    feat(select): deterministic select (median-of-medians) + tests

--- src/test/java/algo/select/DeterministicSelectTest.java
+++ src/test/java/algo/select/DeterministicSelectTest.java
@@ -1,4 +1,29 @@
 package algo.select;
 
-public class DeterministicSelectTest {
+import algo.Metrics;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeterministicSelectTest {
+    @Test
+    void matchesSortedKth() {
+        Random rnd = new Random(7);
+        for (int t = 0; t < 50; t++) {
+            int n = 1 + rnd.nextInt(5000);
+            int[] a = rnd.ints(n, -1_000_000, 1_000_000).toArray();
+            int[] b = a.clone();
+            Arrays.sort(b);
+            int k = rnd.nextInt(n);
+
+            Metrics m = new Metrics();
+            DeterministicSelect ds = new DeterministicSelect(m);
+            int v = ds.select(a, k);
+
+            assertEquals(b[k], v);
+        }
+    }
 }
